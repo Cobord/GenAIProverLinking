92a88f5ba42aace6335fd1b9773297ebd18859e8
Cobord <ammar.s.husain@gmail.com>
2024-04-23 17:33:45 -0400

    benchify

---
+++ benchify_test.py
@@ -0,0 +1,14 @@
+"""
+trying out benchify
+https://www.benchify.ai/
+"""
+
+from typing import List, Optional
+
+def index_of_max(_ls : List[int]) -> Optional[int]:
+    """
+    return the index of the maximum element
+    and None for empty list
+    """
+    max_seen = None
+    return max_seen
